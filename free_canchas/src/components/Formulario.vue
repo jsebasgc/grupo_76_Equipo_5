465bd68d4f4f27e94d57f11f73f9e74f8774c92e
Nicolas <nicksiuxs@gmail.com>
2021-09-26 20:26:33 -0500

    ADD. create cancha page

--- free_canchas/src/components/Formulario.vue
+++ free_canchas/src/components/Formulario.vue
@@ -1,75 +1,72 @@
-    <template>
-    <div>
-    <h1>Formulario</h1>
-    <v-form
+<template>
+    <div class="form">
+        <h1 class="form__title">Crear Cancha</h1>
+        <v-form
         ref="form"
         v-model="valid"
         lazy-validation
-    >
-        <v-text-field
-        v-model="name"
-        :counter="10"
-        :rules="nameRules"
-        label="Name"
-        required
-        ></v-text-field>
-
-        <v-text-field
-        v-model="email"
-        :rules="emailRules"
-        label="E-mail"
-        required
-        ></v-text-field>
-
-        <v-select
-        v-model="select"
-        :items="items"
-        :rules="[v => !!v || 'Item is required']"
-        label="Item"
-        required
-        ></v-select>
-
-        <v-checkbox
-        v-model="checkbox"
-        :rules="[v => !!v || 'You must agree to continue!']"
-        label="Do you agree?"
-        required
-        ></v-checkbox>
-
-        <v-btn
-        :disabled="!valid"
-        color="success"
-        class="mr-4"
-        @click="validate"
         >
-        Validate
-        </v-btn>
+            <v-text-field
+            v-model="name"
+            label="Nombre del complejo deportivo"
+            required
+            ></v-text-field>
 
-        <v-btn
-        color="error"
-        class="mr-4"
-        @click="reset"
-        >
-        Reset Form
-        </v-btn>
+            <v-text-field
+            label="Número de la cancha"
+            required
+            ></v-text-field>
 
-        <v-btn
-        color="warning"
-        @click="resetValidation"
-        >
-        Reset Validation
-        </v-btn>
-    </v-form>
+            <v-checkbox
+            v-model="checkbox"
+            label="Está disponible"
+            ></v-checkbox>
+
+            <v-text-field
+            label="Precio fines de semana"
+            required
+            ></v-text-field>
+
+            <v-text-field
+            label="Precio entre semana"
+            required
+            ></v-text-field>
+
+
+            <v-btn
+            color="success"
+            class="mr-4"
+            @click="crear"
+            >
+            Guardar
+            </v-btn>
+        </v-form>
     </div>
-    </template>
+</template>
 
 <script>
 export default {
     name: 'crear',
-
+    methods:{
+        atras(){
+            //Redirigir a las canchas
+            this.$router.push('/home');
+        },
+        crear(){
+            alert("Cancha creada con éxito")
+        }
+    }
 }
 </script>
 
 <style>
+.form{
+    display: flex;
+    flex-direction: column;
+    padding: 20px 30px 20px 30px;
+}
 
+.form__title{
+    text-align: center;
+}
 </style>
--- free_canchas/src/router/index.js
+++ free_canchas/src/router/index.js
@@ -1,34 +1,32 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Login from '../views/Login.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Login from "../views/Login.vue";
+import CreateCancha from "../views/CreateCancha.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'Login',
-    component: Login
-  },
-  {
-    path: '/home',
-    name: 'Home',
-    component: ()=> import('../views/Home.vue')
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+	{
+		path: "/",
+		name: "Login",
+		component: Login,
+	},
+	{
+		path: "/home",
+		name: "Home",
+		component: () => import("../views/Home.vue"),
+	},
+	{
+		path: "/crear",
+		name: "Crear Cancha",
+		component: CreateCancha,
+	},
+];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+	mode: "history",
+	base: process.env.BASE_URL,
+	routes,
+});
 
-export default router
+export default router;
