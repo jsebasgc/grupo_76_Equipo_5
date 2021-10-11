786457b3c970ad166f01f77ebd04587528743494
Nicolas <nicksiuxs@gmail.com>
2021-10-10 22:56:15 -0500

    ADD: new conection db

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    db: "mongodb://localhost:27017/free_canchas"
-}
+  // db: "mongodb://localhost:27017/free_canchas"
+  db: "mongodb+srv://p76g5:free_canchas@cluster0.2y7lg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+};
