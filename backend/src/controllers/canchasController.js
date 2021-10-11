b7ba1cd79f747cc71f63a6f02ff6d969596ce609
Nicolas <nicksiuxs@gmail.com>
2021-10-10 19:25:23 -0500

    REFACTOR: get all canchas

--- backend/src/controllers/canchasController.js
+++ backend/src/controllers/canchasController.js
@@ -1,68 +1,67 @@
-const { json } = require("express");
 const canchas = require("../models/canchas");
 
+class CanchasController {
+  constructor() {}
 
-class CanchasController{
-    constructor(){
+  //Registrar carro
+  register(req, res) {
+    canchas.create(req.body, (error, data) => {
+      if (error) {
+        res.status(500).json({ error });
+      } else {
+        res.status(201).json(data);
+      }
+    });
+  }
 
-    }
-
-    //Registrar carro
-    register(req, res){
-        canchas.create(req.body, (error, data)=>{
-            if(error){
-                res.status(500).json({error});
-            }else{
-                res.status(201).json(data);
-            }
-        });
-    }
-
-    //Actualizar carro
-    update(req, res){
-        let {id, complejoDeportivo, numCancha, disponibilidad, phFds, phEs} = req.body;
-        let objCancha = {
-            complejoDeportivo,
-            numCancha,
-            disponibilidad,
-            phFds,
-            phEs
+  //Actualizar carro
+  update(req, res) {
+    let { id, complejoDeportivo, numCancha, disponibilidad, phFds, phEs } =
+      req.body;
+    let objCancha = {
+      complejoDeportivo,
+      numCancha,
+      disponibilidad,
+      phFds,
+      phEs,
+    };
+    canchas.findByIdAndUpdate(
+      id,
+      {
+        $set: objCancha,
+      },
+      (error, data) => {
+        if (error) {
+          res.status(500).send();
+        } else {
+          res.status(200).json(data);
         }
-        canchas.findByIdAndUpdate(id, {
-            $set: objCancha
-        },(error, data)=>{
-            if(error){
-                res.status(500).send();
-            }else{
-                res.status(200).json(data);
-            }
-        });
-    }
+      }
+    );
+  }
 
-    //Eliminar un carro
-    delete(req, res){
-        let {id} = req.body;
-        canchas.findByIdAndRemove(id, (error, data)=>{
-            if(error){
-                res.status(500).send();
-            }else{
-                res.status(200).json(data);
-            }
-        });
-    }
+  //Eliminar un carro
+  delete(req, res) {
+    let { id } = req.body;
+    canchas.findByIdAndRemove(id, (error, data) => {
+      if (error) {
+        res.status(500).send();
+      } else {
+        res.status(200).json(data);
+      }
+    });
+  }
 
-    //Consultar carros
-    getCanchas(req, res){
-        canchas.find((error, data) =>{
-            if(error){
-                res.status(500).send();
-            }else{
-                res.status(200).json(data);
-            }
-        });
-    }  
-
-    
+  //Consultar carros
+  getAllCanchas(req, res) {
+    canchas.find((error, data) => {
+      if (error) {
+        res.status(500).send();
+      } else {
+        res.status(200).json(data);
+      }
+    });
+  }
 }
 
 module.exports = CanchasController;
